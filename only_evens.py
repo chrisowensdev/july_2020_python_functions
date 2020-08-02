cff5e83a351bf47667136ef6070950a338653957
Chris Owens <guitarrockstarco@gmail.com>
2020-08-02 18:21:14 -0400

    Add is_even function to file to find only evens

--- only_evens.py
+++ only_evens.py
@@ -1,10 +1,17 @@
 list1 = [11, 20, 42, 97, 23, 10]
 
 
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def only_evens(lst):
     even_list = []
     for num in lst:
-        if num % 2 == 0:
+        if is_even(num):
             even_list.append(num)
     return even_list
 
